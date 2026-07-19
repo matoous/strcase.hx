aa6974bf3c6bc3123edfde984892bfb8dbe806c1
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-07-19 17:08:50 +0200

    feat: ci/cd, release setup

--- cog.scm
+++ cog.scm
@@ -1,6 +1,6 @@
 (define package-name 'strcase.hx)
 
-(define version "0.1.0")
+(define version "0.1.0") ;; x-release-please-version
 (define dependencies '())
 
 (define entrypoint '(#:name "strcase.hx" #:path "init.scm"))
